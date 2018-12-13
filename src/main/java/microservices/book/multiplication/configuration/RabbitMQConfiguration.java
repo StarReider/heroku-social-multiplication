18770e446969f574afca6d8315a523b10bd69e41
Bezugomonnov <Bezugomonnov@WSM-0768.netcracker.com>
2018-12-13 20:08:57 +0300

    rabbitmq

--- src/main/java/microservices/book/multiplication/configuration/RabbitMQConfiguration.java
+++ src/main/java/microservices/book/multiplication/configuration/RabbitMQConfiguration.java
@@ -11,10 +11,13 @@ import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableAutoConfiguration(exclude=RabbitAutoConfiguration.class)
 public class RabbitMQConfiguration {
 
 	@Bean
