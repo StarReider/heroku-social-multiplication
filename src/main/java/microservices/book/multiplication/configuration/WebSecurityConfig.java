1b7521146cdd0585b44d4bb96ce646e5b2e72d05
StarReider <star15@bk.ru>
2019-04-15 23:56:58 +0300

    dummy commit

--- src/main/java/microservices/book/multiplication/configuration/WebSecurityConfig.java
+++ src/main/java/microservices/book/multiplication/configuration/WebSecurityConfig.java
@@ -1,15 +1,40 @@
 package microservices.book.multiplication.configuration;
 
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.requiresChannel()
-	      .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
-	      .requiresSecure();
+		http.cors().and().csrf().disable()
+			//if X-Forwarded-Proto then use https (heroku)
+		    .requiresChannel()
+	        .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
+	        .requiresSecure();
 	}
+	
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		//configuration.setAllowCredentials(true);
+		configuration.setAllowedOrigins(Arrays.asList(CorsConfiguration.ALL));
+		configuration.setAllowedMethods(Arrays.asList(HttpMethod.GET.name(), HttpMethod.PUT.name(), 
+													  HttpMethod.POST.name(), HttpMethod.DELETE.name()));
+		configuration.setAllowedHeaders(Arrays.asList(CorsConfiguration.ALL));
+		
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		
+		return source;
+	}
+
 }
